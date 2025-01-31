1dae79f9ec9712a984b00b95309edc6cf511db5e
Noy <noyhillel@gmail.com>
2025-01-31 13:25:59 +0000

    Update MailgunAPI.swift

--- Mailgun-In-Swift/Api/MailgunAPI.swift
+++ Mailgun-In-Swift/Api/MailgunAPI.swift
@@ -5,15 +5,19 @@
 //  Created by Ao Zhang on 21/11/2017.
 //  Copyright © 2017 KevinAo. All rights reserved.
 //
+// EDITED TO WORK WITH ALAMOFIRE 5/6 by https://github.com/Noy
+//
 
 import Alamofire
 import ObjectMapper
+import Foundation
 
 /// Mailgun API Class to be use to send emails
 open class MailgunAPI {
     
     fileprivate let apiKey : String
     fileprivate let domain : String
+    fileprivate let regionEurope : Bool
     
     
     //ApiRouter enum that will take care of the routing of the urls and paths of the API
@@ -30,10 +34,15 @@ open class MailgunAPI {
             }
         }
         
-        fileprivate func urlStringWithApiKey(_ apiKey : String) -> URLConvertible {
+        fileprivate func urlStringWithApiKey(_ apiKey : String, regionEurope: Bool) -> URLConvertible {
             
             //Builds the url with the API key
-            let urlWithKey = "https://api:\(apiKey)@\(Constants.mailgunApiURL)"
+            let urlWithKey: String
+            if regionEurope {
+                urlWithKey = "https://api:\(apiKey)@\(Constants.mailgunApiURLEU)"
+            } else {
+                urlWithKey = "https://api:\(apiKey)@\(Constants.mailgunApiURL)"
+            }
             //Build API URL
             var url = URL(string: urlWithKey)!
             url = url.appendingPathComponent(path)
@@ -51,14 +60,14 @@ open class MailgunAPI {
      
      - parameter apiKey:       Api key to use the API
      - parameter clientDomain: Client domain authorized to send your emails
+     - parameter regionEurope: Client region, default is not europe
      
      - returns: MailGun API Object
      */
-    public init(apiKey:String, clientDomain:String)
-    {
+    public init(apiKey:String, clientDomain:String, regionEurope:Bool = false){
         self.apiKey = apiKey
         self.domain = clientDomain
-        
+        self.regionEurope = regionEurope
     }
     
     
@@ -85,81 +94,69 @@ open class MailgunAPI {
      - parameter email:             email object
      - parameter completionHandler: completion handler
      */
-    open func sendEmail(_ email: MailgunEmail, completionHandler:@escaping (MailgunResult)-> Void) -> Void {
-        
-        /// Serialize the object to an dictionary of [String:Anyobject]
+    
+    open func sendEmail(_ email: MailgunEmail, completionHandler: @escaping (MailgunResult) -> Void) -> Void {
+        // Serialize the object to a dictionary [String: Any]
         let params = Mapper().toJSON(email)
+
+        // The Mailgun API expects multipart params
+        // Set up the multipart request
         
-        //The mailgun API expect multipart params.
-        //Setups the multipart request
-        Alamofire.upload(multipartFormData: { multipartFormData in
-            
-            // add parameters as multipart form data to the body
+        let headers: HTTPHeaders = [
+            .authorization(username: "api", password: self.apiKey)
+        ]
+        
+        AF.upload(multipartFormData: { multipartFormData in
             for (key, value) in params {
-                
-                multipartFormData.append((value as! String).data(using: .utf8, allowLossyConversion: false)!, withName: key)
-            }
-            
-        }, to: ApiRouter.sendEmail(self.domain).urlStringWithApiKey(self.apiKey), encodingCompletion: { encodingResult in
-            switch encodingResult {
-            //Check if it works
-            case .success(let upload, _, _):
-                upload.responseJSON { response in
-                    
-                    //Check the response
-                    switch response.result{
-                        
-                    case .failure(let error):
-                        
-                        print("error calling \(ApiRouter.sendEmail)")
-                        
-                        let errorMessage = error.localizedDescription
-                        
-                        if let data = response.data
-                        {
-                            let errorData = String(data: data, encoding: String.Encoding.utf8)
-                            print(errorData as Any)
-                        }
-                        
-                        let result = MailgunResult(success: false, message: errorMessage, id: nil)
-                        
-                        completionHandler(result)
-                        return
-                        
-                    case .success:
-                        
-                        if let value: AnyObject = response.result.value as AnyObject? {
-                            
-                            let result:MailgunResult = ObjectParser.objectFromJson(value)!
-                            
-                            result.success = true
-                            
-                            completionHandler(result)
-                            
-                            return
-                            
-                        }
-                        
+                if let stringValue = value as? String {
+                    if let data = stringValue.data(using: .utf8, allowLossyConversion: false) {
+                        multipartFormData.append(data, withName: key)
                     }
                 }
-            //Check if we fail
+            }
+        }, to: ApiRouter.sendEmail(self.domain).urlStringWithApiKey(self.apiKey, regionEurope: self.regionEurope), headers: headers)
+        .validate()
+        .responseDecodable(of: MailgunResult.self) { response in
+            switch response.result {
+            case .success(let result):
+                // Check if the response contains a success message
+                if let message = result.message?.lowercased(), message.contains("queued") {
+                    // Treat it as success if the message contains 'Queued' (which mailgun does)
+                    print("Email successfully queued: \(message)")
+                    completionHandler(result)
+                } else {
+                    // Handle unexpected responses or success in another format
+                    let failureResult = MailgunResult(success: false, message: "Unexpected response format: \(result.message ?? "No message")", id: result.id)
+                    completionHandler(failureResult)
+                }
+
             case .failure(let error):
-                
-                print("error calling \(ApiRouter.sendEmail)")
-                print(error)
-                
-                let errorMessage = "There was an error"
-                
-                let result = MailgunResult(success: false, message: errorMessage, id: nil)
-                
-                completionHandler(result)
-                return
-                
+                // In case of failure, handle the error response
+                print("Error calling Mailgun API: \(error.localizedDescription)")
+
+                // Return failure with error details
+                let failureResult = MailgunResult(success: false, message: error.localizedDescription, id: nil)
+                completionHandler(failureResult)
             }
         }
-        )
     }
+
+    // Helper function to decode JSON into a MailgunResult
+    private func decodeJson(_ json: [String: Any]) -> MailgunResult? {
+        // Convert the dictionary to Data
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: json, options: []) else {
+            return nil
+        }
+        
+        // Decode the JSON into a MailgunResult
+        let decoder = JSONDecoder()
+        do {
+            let result = try decoder.decode(MailgunResult.self, from: jsonData)
+            return result
+        } catch {
+            print("Failed to decode JSON: \(error.localizedDescription)")
+            return nil
+        }
+    }
+
 }
-
-
-
